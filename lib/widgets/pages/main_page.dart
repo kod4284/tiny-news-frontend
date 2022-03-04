640d1384440c5c4769186fe90dfe6c59577975f2
Daewoong Ko <kodw4284@gmail.com>
2022-03-04 14:11:08 -0500

    feat: create search bar

--- lib/widgets/pages/main_page.dart
+++ lib/widgets/pages/main_page.dart
@@ -21,8 +21,9 @@ class _MainPage extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(100.0),
+        preferredSize: const Size.fromHeight(70.0),
         child: AppBar(
+          elevation: 0,
           title: const Image(
             width: 210,
             image: AssetImage('images/tiny_news.png'),
@@ -31,7 +32,38 @@ class _MainPage extends State<MainPage> {
           ),
         ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Stack(
+            children: [
+              Container(
+                height: 20,
+                color: Colors.indigo,
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  height: 45,
+                  width: 370,
+                  child: TextField(
+                    onSubmitted: (e) => {print(e)},
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      fillColor: const Color(0xff65666D),
+                      filled: true,
+                      hintStyle: const TextStyle(color: Color(0xffa7a7a7)),
+                      focusColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      hintText: 'Search articles',
+                    ),
+                  )
+                ),
+              )
+            ],
+          ),
           HorizontalMenu(
             menus: const [
               'All',
@@ -39,7 +71,7 @@ class _MainPage extends State<MainPage> {
               'Entertainment',
               'Health',
               'Lifestyles',
-              'Politices',
+              'Politics',
               'Science',
               'Sports'
             ],
