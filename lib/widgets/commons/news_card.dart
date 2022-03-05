f1e4edf1183b35827b63c51666a675509ff382a8
Haesun Lee <hlee830@gatech.edu>
2022-03-05 18:03:16 -0500

    updated preference and feedback

--- lib/widgets/commons/news_card.dart
+++ lib/widgets/commons/news_card.dart
@@ -21,8 +21,7 @@ class NewsCard extends StatefulWidget {
     required this.cleanUrl,
     required this.date,
     required this.summary,
-  }): super(key: key);
-
+  }) : super(key: key);
 
   @override
   State<NewsCard> createState() => _NewsCard();
@@ -30,7 +29,7 @@ class NewsCard extends StatefulWidget {
 
 class _NewsCard extends State<NewsCard> {
   late bool _isLike;
-  
+
   @override
   void initState() {
     _isLike = widget.isLike;
@@ -46,102 +45,182 @@ class _NewsCard extends State<NewsCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: InkWell(
-          child: Column(
-              children: [
-                Container(
-                    margin: const EdgeInsets.fromLTRB(12, 6, 12, 6),
-                    child: Column(
+      child: InkWell(
+        child: Column(children: [
+          Container(
+              margin: const EdgeInsets.fromLTRB(12, 6, 12, 6),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      widget.thumbnailUrl,
+                      height: 200,
+                      width: 800,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Container(
+                      margin: const EdgeInsets.fromLTRB(6, 7, 6, 7),
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        widget.title,
+                        style: const TextStyle(fontSize: 20),
+                      )),
+                  Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network(
-                            widget.thumbnailUrl,
-                            height: 200,
-                            width: 800,
-                            fit: BoxFit.fill,
-                          ),
-                        ),
                         Container(
-                            margin: const EdgeInsets.fromLTRB(6, 7, 6, 7),
-                            alignment: Alignment.topLeft,
-                            child: Text(widget.title,
-                              style: const TextStyle(
-                                  fontSize: 20
-                              ),
-                            )
-                        ),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                  margin: const EdgeInsets.fromLTRB(6, 7, 6, 3),
-                                  child: Row(
-                                    children: [
-                                      Text(widget.cleanUrl),
-                                      const Text(" • "),
-                                      Text(widget.date)
-                                    ],
-                                  )
-                              ),
-                              Container(
-                                  width: 100,
-                                  height: 22,
-                                  child:Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    children: [
-                                      TinyIconButton(
-                                        icon: _isLike ? Icons.favorite : Icons.favorite_border,
-                                        width: 30,
-                                        height: 30,
-                                        onClick: () => {toggle()},
-                                      ),
-                                      TinyIconButton(
-                                        icon: Icons.share,
-                                        width: 30,
-                                        height: 25,
-                                        onClick: () => {
-                                          ScaffoldMessenger.of(context).showSnackBar(
-                                              const SnackBar(
-                                                content: Text("The news url copied into clipboard"),
-                                              )),
-                                          Clipboard.setData(ClipboardData(text: widget.url))
-                                        },
-                                      ),
-                                      TinyIconButton(
-                                        icon: Icons.feedback,
-                                        width: 30,
-                                        height: 25,
-                                        onClick: () => {},
-                                      ),
-                                    ],
-                                  )
-                              )
-                          ]),
-                      ],
-                    )
-                ),
-                const Divider(
-                  thickness: 2,
-                  color: Colors.white12,
-                ),
-              ]
+                            margin: const EdgeInsets.fromLTRB(6, 7, 6, 3),
+                            child: Row(
+                              children: [
+                                Text(widget.cleanUrl),
+                                const Text(" • "),
+                                Text(widget.date)
+                              ],
+                            )),
+                        Container(
+                            width: 100,
+                            height: 22,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                TinyIconButton(
+                                  icon: _isLike
+                                      ? Icons.favorite
+                                      : Icons.favorite_border,
+                                  width: 30,
+                                  height: 30,
+                                  onClick: () => {toggle()},
+                                ),
+                                TinyIconButton(
+                                  icon: Icons.share,
+                                  width: 30,
+                                  height: 25,
+                                  onClick: () => {
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(const SnackBar(
+                                      content: Text(
+                                          "The news url copied into clipboard"),
+                                    )),
+                                    Clipboard.setData(
+                                        ClipboardData(text: widget.url))
+                                  },
+                                ),
+                                TinyIconButton(
+                                  icon: Icons.feedback,
+                                  width: 30,
+                                  height: 25,
+                                  onClick: () => {_bottomSheet(context)},
+                                ),
+                              ],
+                            ))
+                      ]),
+                ],
+              )),
+          const Divider(
+            thickness: 2,
+            color: Colors.white12,
           ),
-          onTap: () => {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder:
-                  (context) => SummaryPage(
-                    title: widget.title,
-                    summary: widget.summary,
-                    url: widget.url,
-                  )
-              ),
-            )
-          },
-        ),
+        ]),
+        onTap: () => {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => SummaryPage(
+                      title: widget.title,
+                      summary: widget.summary,
+                      url: widget.url,
+                    )),
+          )
+        },
+      ),
     );
+  }
+
+  _bottomSheet(context) {
+    showModalBottomSheet(
+        context: context,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(25.0),
+          ),
+        ),
+        backgroundColor: Color.fromARGB(66, 68, 66, 66).withAlpha(255),
+        builder: (BuildContext c) {
+          return Wrap(
+            children: [
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        '  Preference & Feedback',
+                        style: TextStyle(
+                          height: 2.0,
+                          color: Colors.grey.shade200,
+                          fontSize: 22.0,
+                          //fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.white,
+                      height: 2.0,
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.sentiment_dissatisfied_outlined,
+                          color: Colors.grey.shade400, size: 25.0),
+                      title: Text(
+                        'Not interested in this',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.grey.shade200,
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.sentiment_satisfied_alt,
+                          color: Colors.grey.shade400, size: 25.0),
+                      title: Text(
+                        'Interested in this',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.grey.shade200,
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.warning_amber_rounded,
+                          color: Colors.grey.shade400, size: 25.0),
+                      title: Text(
+                        'Report this',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.grey.shade200,
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.forum_outlined,
+                          color: Colors.grey.shade400, size: 25.0),
+                      title: Text(
+                        'Send feedback',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.grey.shade200,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          );
+        });
   }
 }
