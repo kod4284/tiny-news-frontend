7047f5d683639ebfcc554f132b27cc4abecb5d5e
Haesun Lee <hlee830@gatech.edu>
2022-03-05 17:49:59 -0500

    updated preference and feedback

--- lib/widgets/commons/tiny_icon_button.dart
+++ lib/widgets/commons/tiny_icon_button.dart
@@ -6,32 +6,34 @@ class TinyIconButton extends StatelessWidget {
   final double height;
   final Function onClick;
 
-  const TinyIconButton({
-    Key? key,
-    required this.icon,
-    required this.width,
-    required this.height,
-    required this.onClick
-  }) : super(key: key);
+  const TinyIconButton(
+      {Key? key,
+      required this.icon,
+      required this.width,
+      required this.height,
+      required this.onClick})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: width,
-      height: height,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(50),
-        child : Material(
-          color: Colors.transparent,
-          child : InkWell(
-            child : Padding(
-                padding : const EdgeInsets.all(0),
-                child : Icon(icon, size: width-5, color: Color(0xffa8a7a7),)
+        width: width,
+        height: height,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(50),
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              child: Padding(
+                  padding: const EdgeInsets.all(0),
+                  child: Icon(
+                    icon,
+                    size: width - 5,
+                    color: Color(0xffa8a7a7),
+                  )),
+              onTap: () => onClick(),
             ),
-            onTap : () => onClick(),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
