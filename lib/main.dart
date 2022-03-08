b2ca5e25b6cde1d673b85cb6924fd8d258c06cdb
Daewoong Ko <kodw4284@gmail.com>
2022-03-08 09:55:24 -0500

    bug: fix not showing images for Andorid environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'widgets/pages/main_page.dart';
 
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
 void main() async {
   await dotenv.load(fileName: ".env");
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
